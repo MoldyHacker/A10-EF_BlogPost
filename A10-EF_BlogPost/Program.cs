dd980217112ff24fc6dab53e6cb3fb51083fafaa
cmoldenauer <97994521+cmoldenauer@users.noreply.github.com>
2022-11-02 22:33:53 -0500

    Add project files.

---
+++ A10-EF_BlogPost/Program.cs
@@ -0,0 +1,27 @@
+﻿using ApplicationTemplate;
+using ApplicationTemplate.Services;
+
+namespace A10_EF_BlogPost
+{
+    public class Program
+    {
+        /// <summary>
+        /// Entry point for the MainService.  It is unlikely you will need to update this.
+        /// </summary>
+        /// <param name="args"></param>
+        private static void Main(string[] args)
+        {
+            try
+            {
+                var startup = new Startup();
+                var serviceProvider = startup.ConfigureServices();
+                var service = serviceProvider.GetService<IMainService>();
+
+                service?.Invoke();
+            }
+            catch (Exception e)
+            {
+                Console.Error.WriteLine(e);
+            }
+        }
+    }
